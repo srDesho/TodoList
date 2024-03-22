fed1eaec10672b6d331b8416f1128406c0263e54
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-03-22 19:44:21 -0400

    Configurating methods in the logic and persistence controllers

--- src/main/java/com/cristianml/todolist/logic/LogicController.java
+++ src/main/java/com/cristianml/todolist/logic/LogicController.java
@@ -1,18 +1,37 @@
 package com.cristianml.todolist.logic;
 
+import com.cristianml.todolist.persistence.PersistenceController;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import javax.persistence.Persistence;
 
 public class LogicController {
     
-    public Task findTaskPerId(ArrayList<Task> listTask, int id) {
-        Task taskEncountered = null;
-            for(Task task : listTask) {
-                if (task.getId() == id) {
-                    taskEncountered = task;
-                }
-            }
-        return taskEncountered;
+    PersistenceController persisControl = new PersistenceController();
+    
+    // Task
+    // Create task
+    public void createTask(Task task) {
+        persisControl.createTask(task);
     }
     
+    // Delete Task
+    public void deleteTask(int id) {
+        persisControl.deleteTask(id);
+    }
     
+    // Edit task
+    public void editTask(Task task) {
+        persisControl.editTask(task);
+    }
+    
+    // Bring task
+    public Task bringTask(int id) {
+        return persisControl.bringTask(id);
+    }
+    
+    // Bring taskList
+    public LinkedList<Task> bringTaskList() {
+        return persisControl.bringTaskList();
+    }
 }
--- src/main/java/com/cristianml/todolist/persistence/PersistenceController.java
+++ src/main/java/com/cristianml/todolist/persistence/PersistenceController.java
@@ -1,5 +1,46 @@
 package com.cristianml.todolist.persistence;
 
-public class PersistenceController {
+import com.cristianml.todolist.logic.Task;
+import com.cristianml.todolist.persistence.exceptions.NonexistentEntityException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class PersistenceController {
+    TaskJpaController taskJpa = new TaskJpaController();
+    
+    // Create task
+    public void createTask(Task task) {
+        taskJpa.create(task);
+    }
+    
+    // Delete task
+    public void deleteTask(int id) {
+        try {
+            taskJpa.destroy(id);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(PersistenceController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    // Edit task
+    public void editTask(Task task) {
+        try {
+            taskJpa.edit(task);
+        } catch (Exception ex) {
+            Logger.getLogger(PersistenceController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    // Bring a task
+    public Task bringTask(int id) {
+        return taskJpa.findTask(id);
+    }
+    
+    // Bring task List
+    public LinkedList<Task> bringTaskList() {
+        List<Task> list = taskJpa.findTaskEntities();
+        return new LinkedList<>(list);
+    }
 }
